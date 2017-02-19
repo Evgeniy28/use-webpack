849152df3d9e2cf685a80753d21a73e36acb4654
Evgeniy28 <azarov91@mail.ru>
2017-02-19 05:14:14 +0300

    ProvidePlugin

---
+++ frontend/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let users = [
+  {id: 'add', name: 'Vdfgdf'},
+  {id: 'dsa', name: 'Fddfd'},
+  {id: 'fds', name: 'Ggfgf'}
+];
+
+console.log( pluck(users, 'name') );
--- webpack.config.js
+++ webpack.config.js
@@ -1,77 +1,26 @@
 'use strict';
 
-const NODE_ENV = process.env.NODE_ENV || 'development';
 const webpack = require('webpack');
 
 module.exports = {
-  context: __dirname + '/frontend',
-
-  entry: {
-    home: './home',
-    about: './about',
-    common: ['./welcome', './common']
-  },
+  context: './frontend',
+  entry: './app',
 
   output: {
     path: __dirname + '/public',
-    filename: '[name].js',
-    library: '[name]'
+    publicPath: '/',
+    filename: 'app.js',
   },
-
-  watch: NODE_ENV == 'development',
-
-  watchOptions: {
-    aggregateTimeout: 100
-  },
-
-  devtool: NODE_ENV == 'development' ? 'cheap-inline-module-source-map' : 'hidden-source-map',
-
-  plugins: [
-    new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.DefinePlugin({
-      NODE_ENV: JSON.stringify(NODE_ENV),
-      LANG: JSON.stringify('ru'),
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'common'
-    })
-  ],
 
   resolve: {
     modules: ['node_modules'],
     extensions: ['.js']
   },
 
-  resolveLoader: {
-    modules: ['node_modules'],
-    moduleExtensions: ['-loader'],
-    extensions: ['.js']
-  },
-
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel',
-        query: {
-          presets: ['es2015'],
-          plugins: ['transform-runtime']
-        }
-      }
-    ]
-  }
+  plugins: [
+    new webpack.ProvidePlugin({
+      pluck: 'lodash/collection/pluck'
+    })
+  ]
 
 };
-
-if (NODE_ENV == 'production') {
-  module.exports.plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings:     false,
-        drop_console: false,
-        unsafe:       true
-      }
-    })
-  );
-}
